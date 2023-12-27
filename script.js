c34a76007153404bd501f022d42e2a3a204f8fa0
kirti <kirtipamrar504@gmail.com>
2023-12-27 14:44:59 +0530

    Added files

---
+++ script.js
@@ -0,0 +1,14 @@
+gsap.to("#nav", {
+    backgroundColor: "#000",
+    height: "130px",
+    duration: 0.5,
+    scrollTrigger: {
+        trigger: "#nav",
+        scroller: "body",
+        start: "top -10%",
+        end: "top -11%",
+        scrub: 1,
+
+    }
+
+})
